33d691ade7d91e360f67783edd23593b4c8ee932
Vladimir Kuleshov <gottgunst@gmail.com>
2023-07-11 17:31:58 +0300

    feat: first touch

---
+++ src/components/Pages/PageTerms.tsx
@@ -0,0 +1,14 @@
+export function PageTerms() {
+  return (
+    <>
+      <div className="section">
+        <div className="header-compensation"></div>
+      </div>
+
+      <div className="section">
+        <h1 className="section__title">Пользовательское соглашение</h1>
+      </div>
+    </>
+  );
+}
+export default PageTerms; // as unknown as (props: IPageTermsProps) => JSX.Element;
